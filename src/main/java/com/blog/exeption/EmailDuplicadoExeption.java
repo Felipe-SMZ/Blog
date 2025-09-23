14f9f0bd3daae1fa22dcf037a2a4683709c55580
Felipe <felipeshimizu5@gmail.com>
2025-09-23 16:39:56 -0300

    Criando handlers

--- src/main/java/com/blog/exeption/EmailDuplicadoExeption.java
+++
@@ -1,8 +0,0 @@
-package com.blog.exeption;
-
-public class EmailDuplicadoExeption extends RuntimeException {
-
-    public EmailDuplicadoExeption(String mensagem) {
-        super(mensagem);
-    }
-}
---
+++ src/main/java/com/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.blog.handler;
+
+import com.blog.exception.EmailDuplicadoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailDuplicadoException.class)
+    public ResponseEntity<Map<String, Object>> handleEmailDuplicado(EmailDuplicadoException ex) {
+        Map<String, Object> erro = new HashMap<>();
+        erro.put("timestamp", LocalDateTime.now());
+        erro.put("status", HttpStatus.BAD_REQUEST.value());
+        erro.put("erro", "E-mail duplicado");
+        erro.put("mensagem", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+}
