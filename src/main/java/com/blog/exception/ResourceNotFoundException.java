29a5be4e6e2cbab9aa8f78922c2d19322b55d5bb
Felipe <felipeshimizu5@gmail.com>
2025-10-15 13:40:17 -0300

    feat: ResourceNotFoundException

---
+++ src/main/java/com/blog/exception/ResourceNotFoundException.java
@@ -0,0 +1,40 @@
+package com.blog.exception;
+
+/**
+ * Exceção lançada quando um recurso não é encontrado no banco de dados.
+ * Exemplos: Usuario, Post ou Comentario não existe.
+ * HTTP Status: 404 NOT FOUND
+ */
+public class ResourceNotFoundException extends RuntimeException {
+
+    private final String resourceName;
+    private final String fieldName;
+    private final Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s não encontrado(a) com %s: '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    // Construtor simplificado para mensagens customizadas
+    public ResourceNotFoundException(String message) {
+        super(message);
+        this.resourceName = null;
+        this.fieldName = null;
+        this.fieldValue = null;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+}
