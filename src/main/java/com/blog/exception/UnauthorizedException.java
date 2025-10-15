3df0941ff9b8a98da0bfca0cdd222905316634da
Felipe <felipeshimizu5@gmail.com>
2025-10-15 13:41:33 -0300

    feat: UnauthorizedException

--- src/main/java/com/blog/exception/UnauthorizedException.java
+++ src/main/java/com/blog/exception/UnauthorizedException.java
@@ -1,4 +1,17 @@
 package com.blog.exception;
 
-public class UnauthorizedException {
+/**
+ * Exceção lançada quando as credenciais são inválidas.
+ * Exemplos: Login com senha incorreta, email não existe.
+ * HTTP Status: 401 UNAUTHORIZED
+ */
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException() {
+        super("Credenciais inválidas. Verifique seu email e senha.");
+    }
 }
